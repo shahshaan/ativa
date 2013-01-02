53ddaf9478a7e1ed7c0bb1c1dfd6c732e46fcbb5
Shaan Shah <shahshaan@gmail.com>
2013-01-02 15:23:01 -0600

    changing initial post to be by user with email shaan@perfectsearchmedia.com

--- app/models/project.rb
+++ app/models/project.rb
@@ -22,7 +22,7 @@ class Project < ActiveRecord::Base
   					:message => "Welcome! A project for #{self.name} has been created.",
   					:phase => 'onboarding',
   					:project_id => self.id,
-  					:user_id => User.find_by_email("psm@perfectsearchmedia.com").id
+  					:user_id => User.find_by_email("shaan@perfectsearchmedia.com").id
   	)
   end
 
